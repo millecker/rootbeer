5194f12454dc1d7050bc6040b135cb94698a6580
millecker <martin.illecker@gmail.com>
2014-03-31 17:29:31 +0200

    Fix OnlineCF5 kernel

--- examples/onlinecf5/src/at/illecker/rootbeer/examples/onlinecf5/OnlineCFKernel.java
+++ examples/onlinecf5/src/at/illecker/rootbeer/examples/onlinecf5/OnlineCFKernel.java
@@ -122,7 +122,9 @@ public class OnlineCFKernel implements Kernel {
         if (userId < m_N) {
 
           // Each user loops over all items which have a rating
-          for (int itemId = 0; itemId < m_userHelper[userId][0]; itemId++) {
+          for (int itemIdxx = 1; itemIdxx <= m_userHelper[userId][0]; itemIdxx++) {
+
+            int itemId = m_userHelper[userId][itemIdxx];
 
             // Each thread within a block computes one multiplication
             if (thread_idxx < m_matrixRank) {
@@ -159,7 +161,7 @@ public class OnlineCFKernel implements Kernel {
               m_usersMatrix[userId][thread_idxx] += m_itemsMatrix[itemId][thread_idxx]
                   * 2
                   * m_ALPHA
-                  * (m_userItemMatrix[userId][m_userHelper[userId][itemId]] - RootbeerGpu
+                  * (m_userItemMatrix[userId][itemId] - RootbeerGpu
                       .getSharedDouble(shmMultVectorStartPos));
             }
 
@@ -185,7 +187,9 @@ public class OnlineCFKernel implements Kernel {
         if (itemId < m_M) {
 
           // Each user loops over all users which have a rating
-          for (int userId = 0; userId < m_itemHelper[itemId][0]; userId++) {
+          for (int userIdxx = 1; userIdxx <= m_itemHelper[itemId][0]; userIdxx++) {
+
+            int userId = m_itemHelper[itemId][userIdxx];
 
             // Each thread within a block computes one multiplication
             if (thread_idxx < m_matrixRank) {
@@ -222,7 +226,7 @@ public class OnlineCFKernel implements Kernel {
               m_itemsMatrix[itemId][thread_idxx] += m_usersMatrix[userId][thread_idxx]
                   * 2
                   * m_ALPHA
-                  * (m_userItemMatrix[userId][m_itemHelper[itemId][userId]] - RootbeerGpu
+                  * (m_userItemMatrix[userId][itemId] - RootbeerGpu
                       .getSharedDouble(shmMultVectorStartPos));
             }
 
